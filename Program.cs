026a204c0c93d90043075fe229445547499e78e0
Akset045 <akset045@gmail.com>
2022-04-26 01:06:26 +0300

    sone stuff

--- Program.cs
+++ Program.cs
@@ -4,72 +4,44 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Globalization;
 
-namespace App
-{
+namespace App {
 
 	class MyUnit2 {
 
-		
-		static public void Main (string[] args) {
+    // public static int[] CreateAAut(){ // создаёт массив авто
 
-			NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
-			{
-				NumberDecimalSeparator = ".",
-			};
+		// 	Console.Write("Размер массива: ");
+		// 	int mastsize = int.Parse(Console.ReadLine());
 
-			Console.OutputEncoding = System.Text.Encoding.UTF8;
+		// 	var newA = new Random ();
+    // 		var mast = new int[mastsize];
 
-			Console.Write("Размер массива: ");
-			int mastsize = int.Parse(Console.ReadLine());
+    // 		for (int a = 0; a < mast.Length; a++){
+    //     	mast[a] = newA.Next(mastsize);
+    // 		}
+		// 	ShowAAll(mast);
 
-			int[] mast = new int[mastsize];
+		// 	return mast;
 
-			for (int a = 0; a < mast.Length; a++)
-			{
-				Console.Write($"Введите элемент №{a}: ");
-				mast[a] = int.Parse(Console.ReadLine());
-			}
+		// }
+
+		// public static int[] CreateAMan(){ // создаём массив в ручную
+
+		// 	Console.Write("Размер массива: ");
+		// 	int mastsize = int.Parse(Console.ReadLine());
+
+    //   int[] mast = new int[mastsize];
+    //   for (int a = 0; a < mast.Length; a++){
+    //     Console.Write($"Введите элемент №{a}: ");
+    //     mast[a] = int.Parse(Console.ReadLine());
+		// 	}
+		// 	return mast;
+		// }
+
+		public static void ShowAAll(int[] mast) {
 
 			Console.WriteLine($"\nИтоговый массив:");
-
-			for (int a = 0; a < mast.Length; a++)
-			{
-				Console.Write($"{mast[a]}");
-				if (a < mast.Length - 1)
-				{
-					Console.Write("; ");
-				}
-				else if (a == mast.Length - 1)
-				{
-					Console.Write(".");
-				}
-			}
-
-			foreach (int a in mast)
-			{
-				Console.Write($"{a}");
-			}
-
-			Console.ReadKey();
-		}
-
-		public static int[] CreateArray(){ // создаёт массив
-
-            Console.Write("Размер массива: ");
-            int mastsize = int.Parse(Console.ReadLine());
-
-            int[] mast = new int[mastsize];
-
-            for (int a = 0; a < mast.Length; a++)
-            {
-                Console.Write($"Введите элемент №{a}: ");
-                mast[a] = int.Parse(Console.ReadLine());
-            }
-
-            Console.WriteLine($"\nИтоговый массив:");
-
-			for (int a = 0; a < mast.Length; a++)
-			{
+			for (int a = 0; a < mast.Length; a++){
 				Console.Write($"{mast[a]}");
 				if (a < mast.Length-1){
 					Console.Write("; ");
@@ -79,49 +51,96 @@ namespace App
 				}
 			}
 
+		}
+
+		public static void ShowAUlt(int[]mast) {
+
 			Console.Write($"\nМаксимум: {mast.Max()}; Минимум: {mast.Min()}");
-
-			return mast;
-		   
 		}
 
-		public static void ShowMast(int[] array) {
-
-			foreach (int a in array) {
-
-				Console.Write($"{0}", a);
-			}
-
-		}
-
-		public void SortBubble(int[] array){ // примает массив
-
-			for (int a = 0; a < array.Length ; a++) { // сортировка пузырьком
-				for (int b = 0; b < array.Length-1-a; b++) {
-
-					if (array[b] > array[b++]) {
-						int c = array[b];
-						array[b] = array[b++];
-						array[b++] = c;
-
+		public void SortBubble(int[] mast){ // примает массив
+			for (int a = 0; a < mast.Length ; a++) { // сортировка пузырьком
+				for (int b = 0; b < mast.Length-1-a; b++) {
+					if (mast[b] > mast[b++]) {
+						int c = mast[b];
+						mast[b] = mast[b++];
+						mast[b++] = c;
 					}
 				}
-				
-			}
-
-		}
-
-		static void Fart(string[] args) {
-
-			string? ant = Console.ReadLine();
-			int bant = Int32.Parse(ant);
-			int anx = Int32.Parse(ant);
-
-			for (; anx < 1000; anx++) {
-
-				bant++;
-				Console.WriteLine($"{bant}");
 			}
 		}
+
+		// static void Fart(string[] args) {
+
+		// 	string? ant = Console.ReadLine();
+		// 	int bant = Int32.Parse(ant);
+		// 	int anx = Int32.Parse(ant);
+
+		// 	for (; anx < 1000; anx++) {
+
+		// 		bant++;
+		// 		Console.WriteLine($"{bant}");
+		// 	}
+		// }
+
+		static public void Main()
+		{
+
+			NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+			{
+				NumberDecimalSeparator = "."
+			};
+
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+			static int[] CreateAAut(){ // создаёт массив авто
+
+				Console.Write("Размер массива: ");
+				int mastsize = int.Parse(Console.ReadLine());
+
+				var newA = new Random();
+				var mast = new int[mastsize];
+
+				for (int a = 0; a < mast.Length; a++){
+					mast[a] = newA.Next(mastsize);
+				}
+				ShowAAll(mast);
+
+				return mast;
+
+			}
+
+			static int[] CreateAMan(){ // создаём массив в ручную
+
+				Console.Write("Размер массива: ");
+				int mastsize = int.Parse(Console.ReadLine());
+
+				int[] mast = new int[mastsize];
+				for (int a = 0; a < mast.Length; a++){
+					Console.Write($"Введите элемент №{a}: ");
+					mast[a] = int.Parse(Console.ReadLine());
+				}
+				return mast;
+			}
+
+			switch(Console.ReadLine())
+			{
+				case "1":
+					CreateAAut();
+					break;
+
+				case "2":
+					CreateAMan();
+					break;
+
+				default:
+					Console.WriteLine("Что-то другое?");
+					break;
+
+			}
+
+			Console.ReadKey();
+		}
+
 	}
 }
